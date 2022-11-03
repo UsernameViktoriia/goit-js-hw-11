adaa401491774aee491e3073ad851d30d5c5ad1a
UsernameViktoriia <tori-boss@ukr.net>
2022-11-03 17:35:45 +0200

    виконано дз

--- src/api.js
+++ src/api.js
@@ -3,10 +3,8 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const keyApi = '31020563-d44473fd087eb3f9b37189b03';
 
-export function fetchImages(input, page) {
-  console.log(input);
-
-  return axios.get(
+export async function fetchImages(input, page) {
+  return await axios.get(
     `/?key=${keyApi}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,34 +8,68 @@ const btnLoadMore = document.querySelector('.load-more');
 const cards = document.querySelector('.gallery');
 let inputSearch = '';
 let page = 1;
+let totalImages = 0;
+let lightbox = new SimpleLightbox('.gallery a');
 
 btnLoadMore.classList.add('is-hidden');
 
-formSearch.addEventListener('submit', e => {
+formSearch.addEventListener('submit', onSubmitForm);
+btnLoadMore.addEventListener('click', onClickLoadMore);
+
+async function onSubmitForm(e) {
   e.preventDefault();
   cards.innerHTML = '';
   inputSearch = e.currentTarget.searchQuery.value;
   if (inputSearch === '') {
-    Notiflix.Notify.failure(
+    return Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
   page = 1;
-  fetchImages(inputSearch, page).then(response =>
-    addMarkup(response.data.hits)
-  );
-  btnLoadMore.classList.remove('is-hidden');
-});
+  try {
+    const { data } = await fetchImages(inputSearch, page);
+    if (!data.hits.length) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+    addMarkup(data.hits);
+    lightbox.refresh();
+    totalImages = data.hits.length;
+    if (totalImages === data.totalHits) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+    if (data.totalHits > 40) {
+      btnLoadMore.classList.remove('is-hidden');
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(`Sorry, ${error.message} `);
+  }
+}
 
-btnLoadMore.addEventListener('click', () => {
+async function onClickLoadMore() {
   page += 1;
-  fetchImages(inputSearch, page).then(response => {
-    // console.log('response');
-    addMarkup(response.data.hits);
-  });
-  let lightbox = new SimpleLightbox('.gallery a');
-  lightbox.refresh();
-});
+  try {
+    const { data } = await fetchImages(inputSearch, page);
+    addMarkup(data.hits);
+    lightbox.refresh();
+    totalImages += data.hits.length;
+    console.log(totalImages);
+    console.log(data.totalHits);
+    if (totalImages == data.totalHits) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      btnLoadMore.classList.add('is-hidden');
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(`Sorry, ${error.message} `);
+  }
+}
 
 function addMarkup(array) {
   const markup = array
@@ -49,25 +83,26 @@ function addMarkup(array) {
         downloads,
         largeImageURL,
       }) => `
-    <a class = "gallery__item" href="${largeImageURL}">
-        
-            <img class = "gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                    <b>Likes ${likes}</b>
-                </p>
-                <p class="info-item">
-                    <b>Views ${views}</b>
-                </p>
-                <p class="info-item">
-                    <b>Comments ${comments}</b>
-                </p>
-                <p class="info-item">
-                    <b>Downloads ${downloads}</b>
-                </p>
-            </div>
-       
-    </a>`
+      <div class="photo-card">
+          <a class = "gallery__item" href="${largeImageURL}">
+            <img class = "gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" width = "400" height="200"/>
+          </a>
+          <div class="info">
+              <p class="info-item">
+                  <b>Likes</b> ${likes}
+              </p>
+              <p class="info-item">
+                  <b>Views</b> ${views}
+              </p>
+              <p class="info-item">
+                  <b>Comments</b> ${comments}
+              </p>
+              <p class="info-item">
+                  <b>Downloads</b> ${downloads}
+              </p>
+          </div>
+       </div>
+    `
     )
     .join('');
   cards.insertAdjacentHTML('beforeend', markup);
